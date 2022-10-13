feda2de99127cdfd138b03227deb9dfb6b2ab97b
Sbilavanh <bilavanh.souphalak@gmail.com>
2022-10-13 13:14:22 -0700

    update

---
+++ final/src/assets/data/blogData.js
@@ -0,0 +1,46 @@
+import img01 from "../all-images/blog-img/blog-1.jpg";
+import img02 from "../all-images/blog-img/blog-2.jpg";
+import img03 from "../all-images/blog-img/blog-3.jpg";
+
+const blogData = [
+    {
+        id: 1,
+        title: "The Art of Attacking corners in certain weather conditions",
+        author: "Souphalak",
+        date: "1 Jan, 2000",
+        time: "12am",
+        imgUrl: img01,
+        description:
+            " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+        quote:
+            "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+    },
+
+    {
+        id: 2,
+        title: "Photos and what the most popular angles are",
+        author: "Souphalak",
+        date: "1 Jan, 2000",
+        time: "12am",
+        imgUrl: img02,
+        description:
+            " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+        quote:
+            "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+    },
+
+    {
+        id: 3,
+        title: "How to look for fun roads to drive on",
+        author: "Souphalak",
+        date: "1 Jan, 2000",
+        time: "12am",
+        imgUrl: img03,
+        description:
+            " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+        quote:
+            "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+    },
+];
+
+export default blogData;
---
+++ final/src/assets/data/carData.js
@@ -0,0 +1,140 @@
+import img01 from "../all-images/cars-img/nissan-offer.png";
+import img02 from "../all-images/cars-img/toyota-supra.png";
+import img03 from "../all-images/cars-img/bmw-offer.png";
+import img04 from "../all-images/cars-img/nissan-offer.png";
+import img05 from "../all-images/cars-img/offer-toyota.png";
+import img06 from "../all-images/cars-img/mercedes-offer.png";
+import img07 from "../all-images/cars-img/toyota-offer-2.png";
+import img08 from "../all-images/cars-img/audi-offer.png";
+
+const carData = [
+    {
+        id: 1,
+        brand: "Tesla",
+        rating: 112,
+        carName: "Tesla Malibu",
+        imgUrl: img01,
+        model: "2017",
+        price: 50,
+        speed: "196MPH",
+        gps: "GPS Navigation",
+        seatType: "Heated seats",
+        automatic: "Automatic",
+        description:
+            " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+    },
+
+    {
+        id: 2,
+        brand: "Toyota",
+        rating: 102,
+        carName: "Toyota Supra",
+        imgUrl: img02,
+        model: "2021",
+        price: 100,
+        speed: "155MPH",
+        gps: "GPS Navigation",
+        seatType: "Heated seats",
+        automatic: "Automatic",
+        description:
+            " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+    },
+
+    {
+        id: 3,
+        brand: "BMW",
+        rating: 132,
+        carName: "BMW 650i GT",
+        imgUrl: img03,
+        model: "2019",
+        price: 100,
+        speed: "155MPH",
+        gps: "GPS Navigation",
+        seatType: "Heated seats",
+        automatic: "Automatic",
+        description:
+            " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+    },
+
+    {
+        id: 4,
+        brand: "Nissan",
+        rating: 102,
+        carName: "Nissan Mercielago",
+        imgUrl: img04,
+        model: "2016",
+        price: 70,
+        speed: "140MPH",
+        gps: "GPS Navigation",
+        seatType: "Heated seats",
+        automatic: "Automatic",
+        description:
+            " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+    },
+
+    {
+        id: 5,
+        brand: "Ferrari",
+        rating: 94,
+        carName: "Ferrari Camry",
+        imgUrl: img05,
+        model: "2016",
+        price: 45,
+        speed: "200MPH",
+        gps: "GPS Navigation",
+        seatType: "Heated seats",
+        automatic: "Automatic",
+        description:
+            " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+    },
+
+    {
+        id: 6,
+        brand: "Mercedes",
+        rating: 119,
+        carName: "Mercedes Benz C-Class",
+        imgUrl: img06,
+        model: "2015",
+        price: 85,
+        speed: "140MPH",
+        gps: "GPS Navigation",
+        seatType: "Heated seats",
+        automatic: "Automatic",
+        description:
+            " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+    },
+
+    {
+        id: 7,
+        brand: "Pagani",
+        rating: 82,
+        carName: "Pagani Zonda R",
+        imgUrl: img07,
+        model: "2011",
+        price: 500,
+        speed: "233MPH",
+        gps: "No Navigational Unit",
+        seatType: "Heated seats",
+        automatic: "Manual",
+        description:
+            " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+    },
+
+    {
+        id: 8,
+        brand: "Audi",
+        rating: 52,
+        carName: "Audi A6 Allroad",
+        imgUrl: img08,
+        model: "2022",
+        price: 100,
+        speed: "129MPH",
+        gps: "GPS Navigation",
+        seatType: "Heated seats",
+        automatic: "Manual",
+        description:
+            " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+    },
+];
+
+export default carData;
